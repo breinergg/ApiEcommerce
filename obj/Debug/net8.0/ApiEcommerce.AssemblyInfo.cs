85a7de16bde907ce95487728291bdc312b29fd01
breinergg <bmgonzalez@unimagdalena.edu.co>
2026-01-06 16:49:11 -0500

    feat: Implement Product and Category management features
    - Added ProductsController to handle product-related API requests.
    - Created ApplicationDbContext with DbSet for Products.
    - Developed AutoMapper profiles for Product and Category DTOs.
    - Implemented Product and Category repository interfaces and their respective classes.
    - Added migration to create Products table in the database.
    - Created models for Product and Category with necessary properties and validation attributes.
    - Added DTOs for creating and updating products and categories.
    - Enhanced Program.cs to register repositories and AutoMapper.
    - Updated CategoryRepository to return nullable Category for GetCategory method.

--- obj/Debug/net8.0/ApiEcommerce.AssemblyInfo.cs
+++ obj/Debug/net8.0/ApiEcommerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiEcommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8da6e884904880117b15500fd060ba175f67a2a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61345f1c3bbe2fde3fdcb71da3c6188e4fa3b145")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiEcommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiEcommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
